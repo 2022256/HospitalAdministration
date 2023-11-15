c7976ce16efbcc92b8c93312742d7e8ccd996656
2022256 <2022256@student.cct.ie>
2023-11-15 14:31:29 +0000

    ttt
    SS

--- src/hospitaladministration/DatabaseWriter.java
+++ src/hospitaladministration/DatabaseWriter.java
@@ -4,10 +4,37 @@
  */
 package hospitaladministration;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.List;
+
 /**
  *
  * @author 4istik
  */
-public class DatabaseWriter {
+public class DatabaseWriter extends Database {
+    
+    public boolean addPatient(Patient patient){
+           try(
+              Connection conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
+              Statement stmt = conn.createStatement();){
+               String sql = String.format("INSERT INTO " + TABLE_NAME + " VALUES ("
+                       + "'%s', '%s', '%s', %d);",
+                       patient.getName(), patient.getBirthDate(), patient.getBloodType(), patient.getID());
+               stmt.execute(sql);
+               return true;
+           } catch (Exception e) {
+               e.printStackTrace();
+               return false;
+           }
+       
+    }
+    
+    public boolean addAllPatients(List <Patient> patientList){
+        
+        return false;
+        
+    }
     
 }
--- src/hospitaladministration/HospitalAdministration.java
+++ src/hospitaladministration/HospitalAdministration.java
@@ -16,8 +16,8 @@ public class HospitalAdministration {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-        Patient IvanPopov = new Patient("15/11/1996","Ivan","A+-="); 
-        Patient Andrei = new Patient("02/06/2000","Andrey","B=_+");
+        Patient IvanPopov = new Patient("1996-11-16","Ivan","A"); 
+        Patient Andrei = new Patient("2000-06-02","Andrei","B");
         
         System.out.println(IvanPopov.getID() +" | "+ IvanPopov.getBirthDate() +" | "+ IvanPopov.getName() +" | "+ IvanPopov.getBloodType());
         System.out.println(Andrei.getID() +" | "+ Andrei.getBirthDate()+" | "+ Andrei.getName() +" | "+ Andrei.getBloodType());
@@ -26,6 +26,17 @@ public class HospitalAdministration {
             System.out.println("Table created");
         }else{
             System.out.println("Problemos muchachos eblanos Samos Dolboebos");
+        }
+        DatabaseWriter dbw = new DatabaseWriter();
+        if(dbw.addPatient(IvanPopov)){
+            System.out.println("Ivan Added");
+        }else{
+            System.out.println("Error");
+        }
+        if(dbw.addPatient(Andrei)){
+            System.out.println("Andrei Added");
+        }else{
+            System.out.println("Error");
         }
     }
     
