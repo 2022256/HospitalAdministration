327c320995a4c66af06abd338719b377c0c5be4e
2022256 <2022256@student.cct.ie>
2023-11-15 13:21:18 +0000

    OSHIBKA

--- src/hospitaladministration/HospitalAdministration.java
+++ src/hospitaladministration/HospitalAdministration.java
@@ -4,6 +4,8 @@
  */
 package hospitaladministration;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author 4istik
@@ -13,12 +15,18 @@ public class HospitalAdministration {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         Patient IvanPopov = new Patient("15/11/1996","Ivan","Red"); 
         Patient Andrei = new Patient("02/06/2000","Andrey","Red");
         
         System.out.println(IvanPopov.getID() + IvanPopov.getDob() + IvanPopov.getName() + IvanPopov.getBloodType());
         System.out.println(Andrei.getID() +" | "+ Andrei.getDob() +" | "+ Andrei.getName() +" | "+ Andrei.getBloodType());
+        
+        if(DatabaseSetup.setupDB()){
+            System.out.println("Table created");
+        }else{
+            System.out.println("Problem");
+        }
     }
     
 }
